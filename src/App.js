7d47781a40c67ab1fc3a1ca4ac5f0e4a134711f7
Rafael Galindo F. <126412980+chachilo@users.noreply.github.com>
2025-03-27 19:26:55 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactGA from 'react-ga4';
 import Contador from './Contador'; // Asegúrate de que la ruta sea correcta
 
-const TRACKING_ID = "G-NEXV1X49S3"; // Reemplaza con tu ID de medición
+const TRACKING_ID = "G-CT5R4HT1FB"; // Reemplaza con tu ID de medición
 
 function App() {
   ReactGA.initialize(TRACKING_ID);
